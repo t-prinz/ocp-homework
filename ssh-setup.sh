d880d1211baf73fad067976db7dd03eb35f6db30
Trey Prinz <tprinz@redhat.com>
2018-01-08 17:29:09 -0500

    updated ssh script

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -14,13 +14,13 @@ for host in ${LB} ${MASTERS} ${INFRA} ${NODES} ${NFS}
 do
   echo "configuring ${host}"
   ssh-copy-id ${host}
-  subscription-manager register --force
-  subscription-manager attach --pool=8a85f98c608ccb070160913e1c1a1cab
-  subscription-manager repos --disable="*"
-  yum-config-manager --disable \*
-  subscription-manager repos \
-    --enable="rhel-7-server-rpms" \
-    --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.7-rpms" \
-    --enable="rhel-7-fast-datapath-rpms"
+#  ssh ${host} "subscription-manager register --force"
+#  ssh ${host} "subscription-manager attach --pool=8a85f9843affb61f013b52c3e53e53a5"
+#  ssh ${host} 'subscription-manager repos --disable="*"'
+#  ssh ${host} "yum-config-manager --disable \*"
+#  ssh ${host} 'subscription-manager repos \
+#    --enable="rhel-7-server-rpms" \
+#    --enable="rhel-7-server-extras-rpms" \
+#    --enable="rhel-7-server-ose-3.7-rpms" \
+#    --enable="rhel-7-fast-datapath-rpms"'
 done
